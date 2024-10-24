c24b31dfff24c29f558f4ce23a4b2240ff37b501
caio <caio@gmail.com>
2024-10-24 19:08:55 -0300

    Judeus

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const port = 3000;
 
 // Primeira Rota
 app.get('/primeira', (req, res) => {
-  res.send('<h1>Bem-vindo à Primeira Rota!</h1>');
+  res.send('<h1>Bem-vindo à Primeira Rota</h1>');
 });
 
 // Segunda rota
 app.get('/segunda', (req, res) => {
-  res.send('<h1>Bem-vindo à Segunda Rota!</h1>');
+  res.send('<h1>Bem-vindo à Segunda Rota</h1>');
 });
 
 // Terceira rota
 app.get('/terceira', (req, res) => {
-  res.send('<h1>Bem-vindo à terceira rota!</h1>');
+  res.send('<h1>Bem-vindo à terceira rota</h1>');
 });
 
 // Iniciar o servidor
